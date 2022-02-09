2b5628b28dfaab463ea267799b47c4e458a1180f
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-02-09 18:55:37 -0500

    commit tree 47da627f1e736f4fad7b273939ab5c286830b5f6

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(firstNum, secondNum) {
+    return (firstNum + secondNum)
+}
+
+function subtract(firstNum, secondNum) {
+    return (firstNum - secondNum)
+}
+
+function multiply(firstNum, secondNum) {
+    return (firstNum * secondNum)
+}
+
+function divide(firstNum, secondNum) {
+    return (firstNum / secondNum)
+}
+
+function increment(n) {
+    return (n += 1)
+}
+
+function decrement(n) {
+    return (n -= 1)
+}
+
+function makeInt(string) {
+    return (parseInt(string, 10))
+}
+
+function preserveDecimal(string) {
+    return (parseFloat(string))
+}
